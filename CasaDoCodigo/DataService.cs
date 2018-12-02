ce913504870d2e10bfd1113a638475eb48d7b72c
luizsd942 <luizsd942@gmail.com>
2018-12-02 21:47:56 -0200

    Criação de Repositório de Produtos; Criando Métodos de Salvar e Listar Produtos; Importando produtos de Json; Criando o DataService para iniciar a Base; Inicializando o Serviço de BD e Repositório de Produto no Startup;

---
+++ CasaDoCodigo/DataService.cs
@@ -0,0 +1,35 @@
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Repositories;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CasaDoCodigo
+{
+    public class DataService : IDataService
+    {
+        private readonly ApplicationContext contexto;
+        private readonly IProdutoRepository produtoRepository;
+
+        public DataService(ApplicationContext contexto, IProdutoRepository produtoRepository)
+        {
+            this.contexto = contexto;
+            this.produtoRepository = produtoRepository;
+        }
+
+        public void InicializaDB()
+        {
+            contexto.Database.EnsureCreated();
+            //List<Livro> livros = GetLivros();
+            //produtoRepository.SaveProdutos(livros);
+        }
+
+        private static List<Livro> GetLivros()
+        {
+            //importando livros de Json para o sistema - aula 3.3
+            var _json = File.ReadAllText("livros.json");
+            var livros = JsonConvert.DeserializeObject<List<Livro>>(_json);
+            return livros;
+        }
+    }
+}
